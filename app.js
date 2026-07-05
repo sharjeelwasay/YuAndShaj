41fd629e88c68dbb607d458a8bc1ba98f1fbd7db
sharjeelwasay <sharjeelwasay@gmail.com>
2026-07-05 14:29:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function goToLogin() {
   window.location.href = "login.html";
 }
+
+// LOGIN FUNCTION
+function login() {
+  const password = document.getElementById("password").value;
+  const email = "wedding@yuandshaj.com";
+
+  const errorEl = document.getElementById("error");
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "upload.html";
+    })
+    .catch((error) => {
+      errorEl.innerText = "Incorrect password. Please try again.";
+    });
+}
